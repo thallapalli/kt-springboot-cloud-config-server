0bd71c2800d1dd80dff53f324f2e3f94fae20644
karnakar <karnakar@ymail.com>
2019-12-30 23:01:48 -0500

    first commit

---
+++ src/main/java/com/kt/springcloud/config/server/KtSpringbootCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.kt.springcloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+public class KtSpringbootCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtSpringbootCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt/springcloud/config/server/KtSpringbootCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.springcloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtSpringbootCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
